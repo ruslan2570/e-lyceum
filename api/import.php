7472bb370117290f35a211fcc40e1ad1416f866b
user <user@name>
2023-07-29 15:34:23 +0300

    add lib

--- api/import.php
+++ api/import.php
@@ -1,8 +1,20 @@
 <?php 
 
+require 'vendor/autoload.php';
+
+
+
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 $file = readfile($_FILES['file']['tmp_name'])
 
+$spreadsheet = new Spreadsheet();
+$activeWorksheet = $spreadsheet->getActiveSheet();
+$activeWorksheet->setCellValue('A1', 'Hello World !');
 
+$writer = new Xlsx($spreadsheet);
+$writer->save('hello world.xlsx');
 
 
 ?>
